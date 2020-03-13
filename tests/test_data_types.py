a3faf7f7678a74cbbab3f174a81831642498a121
Raul Garcia <raul.igs@gmail.com>
2020-03-13 18:56:15 +0000

    Add tests for strings

--- tests/test_data_types.py
+++ tests/test_data_types.py
@@ -5,7 +5,14 @@ from fractions import Fraction
 def test_numbers():
     assert isinstance(1234, int)
     assert isinstance(3.1415, float)
-    assert isinstance(3+4j, complex)
+    assert isinstance(3 + 4j, complex)
     assert isinstance(0b111, int)
     assert isinstance(Decimal(), Decimal)
     assert isinstance(Fraction(), Fraction)
+
+
+def test_strings():
+    assert isinstance('spam', str)
+    assert isinstance("Bob's", str)
+    assert isinstance(b'a\x01c', bytes)
+    assert isinstance(u'sp\xc4m', str)
