ae9a5eb46291d2612408c90c4cc2fe4abf716577
Raul Garcia <raul.igs@gmail.com>
2020-03-18 13:47:20 +0000

    Add test numbers

--- tests/types_and_ops/test_data_types_i.py
+++ tests/types_and_ops/test_data_types_i.py
@@ -2,55 +2,46 @@ from decimal import Decimal
 from fractions import Fraction
 from os import path
 
-# Learning Python. 5th Ed
-# II. Types and Operations
-# 4. Introducing Python Object Types
 
+class TestCoreDataTypes:
 
-def test_numbers():
-    assert isinstance(1234, int)
-    assert isinstance(3.1415, float)
-    assert isinstance(3 + 4j, complex)
-    assert isinstance(0b111, int)
-    assert isinstance(Decimal(), Decimal)
-    assert isinstance(Fraction(), Fraction)
+    def test_numbers(self):
+        assert isinstance(1234, int)
+        assert isinstance(3.1415, float)
+        assert isinstance(3 + 4j, complex)
+        assert isinstance(0b111, int)
+        assert isinstance(Decimal(), Decimal)
+        assert isinstance(Fraction(), Fraction)
 
+    def test_strings(self):
+        assert isinstance('spam', str)
+        assert isinstance("Bob's", str)
+        assert isinstance(b'a\x01c', bytes)
+        assert isinstance(u'sp\xc4m', str)
 
-def test_strings():
-    assert isinstance('spam', str)
-    assert isinstance("Bob's", str)
-    assert isinstance(b'a\x01c', bytes)
-    assert isinstance(u'sp\xc4m', str)
+    def test_lists(self):
+        assert isinstance([1, [2, 'three'], 4.5], list)
+        assert isinstance(list(range(10)), list)
 
+    def test_dictionaries(self):
+        assert isinstance({'food': 'spam', 'taste': 'yum'}, dict)
+        assert isinstance(dict(hours=10), dict)
 
-def test_lists():
-    assert isinstance([1, [2, 'three'], 4.5], list)
-    assert isinstance(list(range(10)), list)
+    def test_files(self):
+        assert path.isfile('../test_file.txt')
 
+    def test_sets(self):
+        assert isinstance(set('abc'), set)
+        assert isinstance({'a', 'b', 'c'}, set)
 
-def test_dictionaries():
-    assert isinstance({'food': 'spam', 'taste': 'yum'}, dict)
-    assert isinstance(dict(hours=10), dict)
+    def test_boolean(self):
+        assert isinstance(True, bool)
+        assert isinstance(False, bool)
 
+    def test_none(self):
+        # https://stackoverflow.com/questions/41928835/how-to-access-the-nonetype-type
+        assert type(None).__name__ == "NoneType"
 
-def test_files():
-    assert path.isfile('../test_file.txt')
-
-
-def test_sets():
-    assert isinstance(set('abc'), set)
-    assert isinstance({'a', 'b', 'c'}, set)
-
-
-def test_boolean():
-    assert isinstance(True, bool)
-    assert isinstance(False, bool)
-
-
-def test_none():
-    # https://stackoverflow.com/questions/41928835/how-to-access-the-nonetype-type
-    assert type(None).__name__ == "NoneType"
-
-# Other core types: types
-# Program unit types: Functions, modules, classes (Part IV, Part V, Part VI)
-# Implementation-related types: Compiled code, stack tracebacks (Part IV, Part VII)
+    # Other core types: types
+    # Program unit types: Functions, modules, classes (Part IV, Part V, Part VI)
+    # Implementation-related types: Compiled code, stack tracebacks (Part IV, Part VII)
---
+++ tests/types_and_ops/test_numbers.py
@@ -0,0 +1,39 @@
+import math
+import random
+
+
+class TestNumbers:
+    def test_addition(self):
+        assert 123 + 222 == 345
+
+    def test_floating_point_multiplication(self):
+        assert 1.5 * 4 == 6.0
+
+    def test_power(self):
+        assert 2 ** 100 == 1267650600228229401496703205376
+        # Python 3.X’s integer type automatically provides extra precision for large numbers like this when needed (in
+        # 2.X, a separate long integer type handles numbers too large for the normal integer type in similar ways)
+
+    def test_floating_point_repr(self):
+        assert 3.1415 * 2 == 6.2830000000000004
+        # This isn’t a bug; this is in the very nature of binary floating-point.
+        # For user-friendly representation: print(3.1415 * 2) == 6.283
+        # See https://docs.python.org/3.0/tutorial/floatingpoint.html for the issues and limitations
+
+    def test_math_pi(self):
+        assert math.pi == 3.141592653589793
+
+    def test_math_sqrt(self):
+        assert math.sqrt(85) == 9.219544457292887
+
+    def test_random_real_value_distribution(self):
+        random_number = random.random()
+        assert random_number < 1.0
+        assert random_number > 0.0
+        # Return the next random floating point number in the range [0.0, 1.0).
+        # https://docs.python.org/3/library/random.html
+
+    def test_random_choice(self):
+        random_choice = random.choice([1, 2, 3, 4])
+        assert random_choice <= 4
+        assert random_choice >= 1
