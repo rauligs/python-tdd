85686df73e0776d1d077c416943ae8fb2340f8ba
Raul Garcia <raul.igs@gmail.com>
2020-03-25 09:40:08 +0000

    Add remaining tuple operation

--- tests/types_and_ops/test_tuples.py
+++ tests/types_and_ops/test_tuples.py
@@ -1,5 +1,6 @@
 class TestTuples:
-    # The tuple object is like a list but immutable.
+    # The tuple object is like a list but immutable. It supports mixed types and nesting,
+    # but they don’t grow and shrink (so 'append', 'pop' or 'remove' are not possible)
     def test_create(self):
         items = (1, 2, 3, 4)
         assert len(items) == 4
@@ -16,3 +17,8 @@ class TestTuples:
     def test_count(self):
         items = (1, 2, 3, 4)
         assert items.count(4) == 1
+
+    def test_create_from_another_tuple(self):
+        items = (1, 2, 3, 4)
+        # one-item tuples like the one here require a trailing comma
+        assert (2,) + items[1:] == (2, 2, 3, 4)
